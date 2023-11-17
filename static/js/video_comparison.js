5c5278af5cbea30441fa03cf8a0826fedb532acb
XPandora <tianyixie77@gmail.com>
2023-11-16 21:54:59 -0800

    make all videos 30fps

--- static/js/video_comparison.js
+++ static/js/video_comparison.js
@@ -1,12 +1,12 @@
 // Written by Dor Verbin, October 2021
 // This is based on: http://thenewcode.com/364/Interactive-Before-and-After-Video-Comparison-in-HTML5-Canvas
 // With additional modifications based on: https://jsfiddle.net/7sk5k4gp/13/
+var position = 0.5;
 
 function playVids(videoId) {
     var videoMerge = document.getElementById(videoId + "Merge");
     var vid = document.getElementById(videoId);
 
-    var position = 0.5;
     var vidWidth = vid.videoWidth/2;
     var vidHeight = vid.videoHeight;
 
@@ -14,7 +14,7 @@ function playVids(videoId) {
 
     
     if (vid.readyState > 3) {
-        vid.play();
+        // vid.play();
 
         function trackLocation(e) {
             // Normalize to [0, 1]
@@ -110,7 +110,7 @@ function resizeAndPlay(element)
   var cv = document.getElementById(element.id + "Merge");
   cv.width = element.videoWidth/2;
   cv.height = element.videoHeight;
-  element.play();
+//   element.play();
   element.style.height = "0px";  // Hide video without stopping it
     
   playVids(element.id);
